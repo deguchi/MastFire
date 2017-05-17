280f0d6052a2e499cef9bbcab6f12b57a5f7cd1c
Satoshi Deguchi <deguchik@gmail.com>
2017-05-17 19:48:42 +0900

    add merge twoot mmls

--- main.js
+++ main.js
@@ -7,11 +7,25 @@
     return playmmlButtonTemplate;
   }
 
-  setTimeout(function () {
-    console.log("DOM fully loaded and parsed");
-    var targetContentsList = document.querySelectorAll('.status__content:not(.ox-playflagged):not(.muted)');
-    console.log(targetContentsList)
-    targetContentsList.forEach(function (targetContent) {
+  var mmls = [];
+
+  // MutationObserver
+  // https://developer.mozilla.org/en-US/docs/Web/API/MutationObserver
+
+  // select the target node
+  var target = document.querySelector('.status-list');
+
+  // create an observer instance
+  var observer = new MutationObserver(function(mutations) {
+
+    // var targetContentsList = document.querySelectorAll('.status__content:not(.ox-playflagged):not(.muted)');
+
+    mutations.forEach(function(mutation) {
+      console.log(mutation);
+      if (mutation.type!=='childList' || mutation.addedNodes.length===0) return;
+      const targetContent = mutation.addedNodes[0].querySelector('.status__content:not(.ox-playflagged):not(.muted)');
+      console.log(targetContent)
+      if (!targetContent) return;
       const sourceText = targetContent.textContent;
       const playmmlButton = playmmlButtonTemplate().cloneNode(true);
 
@@ -21,22 +35,41 @@
       targetContent.appendChild(playmmlAnchor);
       targetContent.classList.add("ox-playflagged");
 
-      // const mml = sourceText.match(/MML@(.*?);/);
-      // console.log(mml)
-      // if (mml) {
+      console.log(sourceText.replace('\n', ''))
+      const mml = sourceText.replace('\n', '').match(/#MML (.*?)\|\|(.*)/);
+      // console.log(sourceText)
+      console.log(mml)
+      if (mml) {
+        const meta = mml[1];
+        const index = meta.match(/INDEX:(.*)/)[1];
+        mmls.push({index: index, mml:mml[2]});
+        console.log(mmls);
         targetContent.parentNode.appendChild(playmmlButton);
         playmmlButton.addEventListener('click', function (e) {
 
             console.log('played');
-            // console.log(mml[0]);
-            playmml(sourceText);
-            // playmml(mml[0]);
-
+            mmls.sort(function(a,b){
+                if(a.index<b.index) return -1;
+                if(a.index > b.index) return 1;
+                return 0;
+            });
+            console.log(mmls);
+            var pmml = [];
+            mmls.map(function (m) {
+              pmml.push(m.mml);
+            });
+            playmml(pmml.join(''));
           }
           , false);
-      // }
+      }
     });
-  }, 1000);
+  });
+
+  // configuration of the observer:
+  var config = { attributes: true, childList: true, characterData: true };
+
+  // pass in the target node, as well as the observer options
+  observer.observe(target, config);
 
   console.log('test')
 
